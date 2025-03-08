97f64d08a1b96dadfa5cefcecf6a1b453abf60e7
shinminchul <su99king@gmail.com>
2025-03-08 16:54:48 +0900

    fix: Scanner,LexicalAnalyzer 리팩토링

--- src/lexical-analyzer/Scanner/scanner.class.h
+++ src/lexical-analyzer/Scanner/scanner.class.h
@@ -7,7 +7,7 @@
 class Scanner {
 public:
     Scanner();
-    std::vector<std::vector<char>> run();
+    std::vector<std::vector<char>> run(const std::string &pythonCode);
 };
 
 #endif // SCANNER_H
